5753c0148e92720dff3164f012ccc21261bfcf15
Loïc Lavaure <loiclavaurepro@gmail.com>
2023-10-19 18:14:01 +0200

    commentaire

--- lib/RDS/connexion.dart
+++ lib/RDS/connexion.dart
@@ -5,5 +5,6 @@ final dio = Dio();
 void getHttp() async {
 //  Response response;
 //  response = await dio.get('https://pokeapi.co/api/v2/pokemon/ditto');
+// hehe drole
   print('un appel comme les autres');
 }
